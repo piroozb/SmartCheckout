18f1eb3ed6d7af3fac429c00315c84f137ff8898
Wu <Stanley.Wu@nuance.com>
2022-09-22 13:57:23 -0400

    Push starter files

--- SmartApp/App.js
+++ SmartApp/App.js
@@ -1,23 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import Login from "./pages/Login";
 
 export default function App() {
   return (
-    <>
-      <SafeAreaView />
-      <SafeAreaView>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Group>
-              <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-            </Stack.Group>
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaView >
-    </>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
